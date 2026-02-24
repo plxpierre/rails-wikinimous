1614c6ac744616d9915a33c27c8e62c9db6e360e
Pierre Planex <pierre.planex@gmail.com>
2026-02-24 15:36:29 +0100

    wiki

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "articles", to: "articles#index"
+
+  # create
+  get "articles/new", to: "articles#new", as: :new_article
+  get "articles/:id", to: "articles#show", as: :article
+  post "articles", to: "articles#create"
+
+  # edit
+  get "articles/:id/edit", to: "articles#edit", as: :edit_article
+  patch "articles/:id", to: "articles#update"
+
+  # delete
+  delete "articles/:id", to: "articles#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+
+require 'faker'
+puts "start"
+Article.destroy_all
+
+10.times do
+    Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(sentence_count: 10)
+  )
+  puts "articles created"
+end
+
+puts "end"
+puts "#{Article.count} created"
